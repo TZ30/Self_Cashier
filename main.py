e0d686fb23d2acc73bf9e0d07f78115eb884543d
TZ30 <99154962+TZ30@users.noreply.github.com>
2024-03-06 12:15:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     while True:
         print("\nPilih menu dibawah ini: ")
         print("1. Tambah Barang")
-        print("2. Hapus Barang")
+        print("2. Hapus barang atau Transaksi")
         print("3. Perbarui Barang")
         print("4. Keranjang Anda")
         print("5. Pembayaran")
@@ -17,78 +17,93 @@ def main():
         choice = input("Masukkan pilihan Anda: ")
 
         if choice == "1":
-            while True:
-                try:
-                    item = str(input("Masukkan nama barang: ")).strip().capitalize()  
-                    quantity = int(input("Masukkan jumlah barang: "))
-                    price = float(input("Masukkan harga barang: "))
-                    var.add_item(item, quantity, price)
-                    print("\nBerhasil menambahkan barang dalam keranjang Anda!")
-                    break
-                except ValueError as e:
-                    print(e)
+            try:
+                item = str(input("Masukkan nama barang: ")).strip().capitalize()  
+                quantity = int(input("Masukkan jumlah barang: "))
+                price = float(input("Masukkan harga barang: "))
+                var.add_item(item, quantity, price)
+                print("\nBerhasil menambahkan barang dalam keranjang Anda!")
+            except ValueError as e:
+                print(e)
                     
         elif choice == "2":
-            try:
-                item = input("Masukkan nama barang: ").capitalize()
-                var.remove_item(item)
-                print("\nBarang telah berhasil dihapus!")
-            except Exception as e:
-                print(e)
+            while True:
+                print("\n Pilihlah menu dibawah in: ")
+                print("1. Hapus barang")
+                print("2. Hapus transaksi")
+                print("3. Keluar\n")
+                remove_choice = input("Masukan pilihan anda: ")
+                if remove_choice == "1":
+                    try:
+                        item = input("Masukkan nama barang: ").capitalize()
+                        var.remove_item(item)
+                        print("\nBarang telah berhasil dihapus!")
+                    except Exception as e:
+                        print(e)
+                elif remove_choice == "2":
+                    var.reset_transaction()
+                    print("\n Transaksi telah berhasil dihapus!")
+
+                elif remove_choice == "3":
+                    break
+
+                else:
+                    print("Pilihan tidak ada!")
 
         elif choice == "3":
-            print("\nPerbarui: ")
-            print("1. Nama Barang")
-            print("2. Jumlah Barang")
-            print("3. Harga Barang")
-            print("4. Keluar")
+            while True:
+                print("\nPerbarui: ")
+                print("1. Nama Barang")
+                print("2. Jumlah Barang")
+                print("3. Harga Barang")
+                print("4. Keluar")
 
-            choice_2 = input("\nMasukkan pilihan Anda: ")
+                update_choice = input("\nMasukkan pilihan Anda: ")
 
-            if choice_2 == "1":  
-                try:
-                    old_item = str(input("Masukkan nama barang yang ingin dirubah: ")).strip().capitalize()
-                    new_item = str(input("Masukkan nama barang yang baru: ")).strip().capitalize()
-                    var.update_item_name(old_item, new_item)
-            
-                    quantity = int(input("Masukkan jumlah barang baru: "))
-                    price = float(input("Masukkan harga barang baru: ")) 
-                    
-                    if new_item in var.items:
-                        if quantity != var.items[new_item][0]:
-                            var.update_quantity(new_item, quantity)
-                    
-                        if price != var.items[new_item][1]:
-                            var.update_price(new_item, price)
-                    
-                    print("\nData barang berhasil diupdate!")      
-                              
-                except Exception as e:
-                    print(e)
+                if update_choice == "1":  
+                    try:
+                        old_item = str(input("Masukkan nama barang yang ingin dirubah: ")).strip().capitalize()
+                        new_item = str(input("Masukkan nama barang yang baru: ")).strip().capitalize()
+                        var.update_item_name(old_item, new_item)
 
-            elif choice_2 == "2":
-                try:
-                    item = input("Masukkan nama barang yang ingin diubah jumlahnya: ").strip().capitalize()
-                    quantity = int(input("Masukkan jumlah barang baru: "))
-                    var.update_quantity(item, quantity)
-                    print("\nJumlah barang berhasil diubah!")
-                except Exception as e:
-                    print(e)
+                        quantity = int(input("Masukkan jumlah barang baru: "))
+                        price = float(input("Masukkan harga barang baru: ")) 
 
-            elif choice_2 == "3":
-                try:
-                    item = input("Masukkan nama barang yang ingin diubah harganya: ").strip().capitalize()
-                    price = float(input("Masukkan harga barang baru: ")) 
-                    var.update_price(item, price)
-                    print("\nHarga barang berhasil diubah!")
-                except Exception as e:
-                    print(e)
+                        if new_item in var.items:
+                            if quantity != var.items[new_item][0]:
+                                var.update_quantity(new_item, quantity)
 
-            elif choice_2 == "4":
-                continue
+                            if price != var.items[new_item][1]:
+                                var.update_price(new_item, price)
 
-            else:
-                print("Pilihan tidak valid. Silakan pilih lagi.")
+                        print("\nData barang berhasil diupdate!")      
+
+                    except Exception as e:
+                        print(e)
+
+                elif update_choice == "2":
+                    try:
+                        item = input("Masukkan nama barang yang ingin diubah jumlahnya: ").strip().capitalize()
+                        quantity = int(input("Masukkan jumlah barang baru: "))
+                        var.update_quantity(item, quantity)
+                        print("\nJumlah barang berhasil diubah!")
+                    except Exception as e:
+                        print(e)
+
+                elif update_choice == "3":
+                    try:
+                        item = input("Masukkan nama barang yang ingin diubah harganya: ").strip().capitalize()
+                        price = float(input("Masukkan harga barang baru: ")) 
+                        var.update_price(item, price)
+                        print("\nHarga barang berhasil diubah!")
+                    except Exception as e:
+                        print(e)
+
+                elif update_choice == "4":
+                    break
+
+                else:
+                    print("Pilihan tidak ada!.")
 
         elif choice == "4":
             print(f"\n--------------- Keranjang Anda, {welcome} ------------------")
